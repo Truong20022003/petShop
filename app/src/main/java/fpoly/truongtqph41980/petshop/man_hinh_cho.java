ae3a8f64d854d20f7df3e70cb4c2b2b2e4ed08c8
Tran Quang Truong <quangtruong20022003@gmail.com>
2023-11-12 02:39:57 +0700

    xoa icon lỗi

--- app/src/main/java/fpoly/truongtqph41980/petshop/man_hinh_cho.java
+++ app/src/main/java/fpoly/truongtqph41980/petshop/man_hinh_cho.java
@@ -2,7 +2,9 @@ package fpoly.truongtqph41980.petshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class man_hinh_cho extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class man_hinh_cho extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_hinh_cho);
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(man_hinh_cho.this, man_hinh_dang_nhap.class);
+                startActivity(intent);
+            }
+        },3000);
     }
 }
