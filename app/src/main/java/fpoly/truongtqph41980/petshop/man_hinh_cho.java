ff0de7cc3a4ce83f43d72b347b633b2887ec2a17
Doan379a <145422399+Doan379a@users.noreply.github.com>
2023-11-11 17:14:43 +0700

    doan commit

---
+++ app/src/main/java/fpoly/truongtqph41980/petshop/man_hinh_cho.java
@@ -0,0 +1,14 @@
+package fpoly.truongtqph41980.petshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class man_hinh_cho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_cho);
+    }
+}
