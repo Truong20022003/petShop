ac7536be6c729b342d1c00700b56f1e71c8c4d9b
Tran Quang Truong <quangtruong20022003@gmail.com>
2023-11-12 20:41:17 +0700

    giao diện đăng ký

---
+++ app/src/main/java/fpoly/truongtqph41980/petshop/man_hinh_dang_ky.java
@@ -0,0 +1,14 @@
+package fpoly.truongtqph41980.petshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class man_hinh_dang_ky extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_dang_ky);
+    }
+}
