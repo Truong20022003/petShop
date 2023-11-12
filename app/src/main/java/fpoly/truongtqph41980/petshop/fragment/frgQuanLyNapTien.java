9912121223e2cbac0018c0dad31a4994ddb794fe
hoang <hoangtnph42058@fpt.edu.vn>
2023-11-12 21:21:12 +0700

    tạo layout fragment

---
+++ app/src/main/java/fpoly/truongtqph41980/petshop/fragment/frgQuanLyNapTien.java
@@ -0,0 +1,28 @@
+package fpoly.truongtqph41980.petshop.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fpoly.truongtqph41980.petshop.R;
+
+
+public class frgQuanLyNapTien extends Fragment {
+
+
+    public frgQuanLyNapTien() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frg_quan_ly_nap_tien, container, false);
+    }
+}
