cfaf1430b6a25152e9b6327054fa2671045a74e4
Doan379a <145422399+Doan379a@users.noreply.github.com>
2023-11-12 22:05:11 +0700

    doan commit

---
+++ app/src/main/java/fpoly/truongtqph41980/petshop/fragment/frgQuanLySanPham.java
@@ -0,0 +1,33 @@
+package fpoly.truongtqph41980.petshop.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import fpoly.truongtqph41980.petshop.R;
+
+
+public class frgQuanLySanPham extends Fragment {
+
+
+
+    public frgQuanLySanPham() {
+        // Required empty public constructor
+    }
+
+
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frg_quan_ly_san_pham, container, false);
+    }
+}
