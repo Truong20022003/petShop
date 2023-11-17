ec6cdd1f96a6f328cc894028f30dabea5d0839c6
Tran Quang Truong <quangtruong20022003@gmail.com>
2023-11-17 21:57:19 +0700

    fix đăng ký, đăng nhập

--- app/src/main/java/fpoly/truongtqph41980/petshop/man_hinh_dang_nhap.java
+++ app/src/main/java/fpoly/truongtqph41980/petshop/man_hinh_dang_nhap.java
@@ -2,27 +2,39 @@ package fpoly.truongtqph41980.petshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 
 import fpoly.truongtqph41980.petshop.Dao.NguoiDungDao;
 import fpoly.truongtqph41980.petshop.databinding.ActivityMainBinding;
 import fpoly.truongtqph41980.petshop.databinding.ActivityManHinhDangNhapBinding;
 
 public class man_hinh_dang_nhap extends AppCompatActivity {
-ActivityManHinhDangNhapBinding binding;
+    ActivityManHinhDangNhapBinding binding;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_hinh_dang_nhap);
         binding = ActivityManHinhDangNhapBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        checkRemember();
         NguoiDungDao nguoiDungDao = new NguoiDungDao(this);
+
         binding.btnDangNhap.setOnClickListener(view -> {
 
             String userName = binding.tiedtTenDangNhap.getText().toString();
             String passWord = binding.tiedtNhapMatKhau.getText().toString();
+
             if (userName.isEmpty()) {
                 binding.tinLTenDangNhap.setError("Không được để trống");
 
@@ -31,13 +43,25 @@ ActivityManHinhDangNhapBinding binding;
                 binding.tipLMatKhau.setError("Không được để trống");
                 return;
             }
-            if (nguoiDungDao.checkDangNhap(userName, passWord)) {
-                Intent intent = new Intent(man_hinh_dang_nhap.this, MainActivity.class);
-                man_hinh_dang_nhap.this.startActivity(intent);
-            } else {
-                binding.tinLTenDangNhap.setError("Tên đăng nhập không đúng");
-                binding.tipLMatKhau.setError("Mật khẩu không đúng");
-            }
+                if (nguoiDungDao.checkDangNhap(userName, passWord)) {
+                    if (binding.chkNhoMatKhau.isChecked()) {
+                        editor.putString("username", userName);
+                        editor.putString("password", passWord);
+                        editor.putBoolean("isChecked", binding.chkNhoMatKhau.isChecked());
+                        editor.apply();
+                    } else {
+                        editor.clear();
+                        editor.apply();
+                    }
+                    binding.tiedtTenDangNhap.setText("");
+                    binding.tiedtNhapMatKhau.setText("");
+                    Intent intent = new Intent(man_hinh_dang_nhap.this, MainActivity.class);
+                    man_hinh_dang_nhap.this.startActivity(intent);
+                    this.finish();
+                } else {
+                    binding.tinLTenDangNhap.setError("Tên đăng nhập không đúng");
+                    binding.tipLMatKhau.setError("Mật khẩu không đúng");
+                }
 
         });
         binding.txtChuyenQuaDangKy.setOnClickListener(new View.OnClickListener() {
@@ -47,5 +71,15 @@ ActivityManHinhDangNhapBinding binding;
                 startActivity(intent);
             }
         });
+    }
+    private void checkRemember() {
+        preferences = this.getSharedPreferences("ACCOUNT", MODE_PRIVATE);
+        editor = preferences.edit(); // gọi dòng trên và edit vào nó
+        boolean isCheck = preferences.getBoolean("isChecked", false);
+        if (isCheck) {
+            binding.tiedtTenDangNhap.setText(preferences.getString("username", ""));
+            binding.tiedtNhapMatKhau.setText(preferences.getString("password", ""));
+            binding.chkNhoMatKhau.setChecked(isCheck);
+        }
     }
 }
