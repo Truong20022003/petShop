0c20eed27f061b27406fda4bd9d835114ed11628
hoang <hoangtnph42058@fpt.edu.vn>
2023-11-21 12:25:44 +0700

    hiển thị profile,fix ql người dùng

--- app/src/main/java/fpoly/truongtqph41980/petshop/Profile.java
+++ app/src/main/java/fpoly/truongtqph41980/petshop/Profile.java
@@ -2,13 +2,39 @@ package fpoly.truongtqph41980.petshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import fpoly.truongtqph41980.petshop.databinding.ActivityProfileBinding;
+
 public class Profile extends AppCompatActivity {
+    ActivityProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        SharedPreferences preferences = getSharedPreferences("NGUOIDUNG", MODE_PRIVATE);
+        int mand = preferences.getInt("mataikhoan", 0);
+        String tenDN = preferences.getString("tendangnhap", "");
+        String matkhau = preferences.getString("matkhau", "");
+        String hoten = preferences.getString("hoten", "");
+        String email = preferences.getString("email", "");
+        String sodienthoai = preferences.getString("sodienthoai", "");
+        String diachi = preferences.getString("diachi", "");
+        int tien = preferences.getInt("sotien", 0);
+        String loaitaikhoan = preferences.getString("loaitaikhoan", "");
+        binding.hiName.setText("Hi, "+hoten);
+        binding.txtPMaNguoiDung.setText("Mã tài khoản: " + String.valueOf(mand));
+        binding.txtPTenDangNhap.setText("Tên đăng nhập: " + tenDN);
+        binding.txtPHoTen.setText("Họ tên: " + hoten);
+        binding.txtPEmail.setText("Email: " + email);
+        binding.txtPSoDienThoai.setText("Số điện thoại: " + sodienthoai);
+        binding.txtPDiaChi.setText("Địa chỉ: " + diachi);
+        binding.txtPSoTien.setText("Số tiền hiện có: " + String.valueOf(tien));
+        binding.txtPLoaiTaiKhoan.setText("Loại tài khoản: " + loaitaikhoan);
     }
 }
--- app/src/main/java/fpoly/truongtqph41980/petshop/fragment/frgQuanLyNguoiDung.java
+++ app/src/main/java/fpoly/truongtqph41980/petshop/fragment/frgQuanLyNguoiDung.java
@@ -24,55 +24,56 @@ import fpoly.truongtqph41980.petshop.databinding.FragmentFrgQuanLyNguoiDungBindi
 public class frgQuanLyNguoiDung extends Fragment {
 
 
-private FragmentFrgQuanLyNguoiDungBinding binding;
-private View vView;
-private NguoiDungDao nguoiDungDao;
-private adapter_nguoi_dung adapterNguoiDung;
-private ArrayList<NguoiDung> list = new ArrayList<>();
+    private FragmentFrgQuanLyNguoiDungBinding binding;
+    private View vView;
+    private NguoiDungDao nguoiDungDao;
+    private adapter_nguoi_dung adapterNguoiDung;
+    private ArrayList<NguoiDung> list = new ArrayList<>();
+
     public frgQuanLyNguoiDung() {
         // Required empty public constructor
     }
-    
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentFrgQuanLyNguoiDungBinding.inflate(inflater,container,false);
+        binding = FragmentFrgQuanLyNguoiDungBinding.inflate(inflater, container, false);
         vView = binding.getRoot();
         nguoiDungDao = new NguoiDungDao(getContext());
         list = nguoiDungDao.getAllNguoiDung();
         RecyclerView rcv = binding.rcvNguoiDung;
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         rcv.setLayoutManager(layoutManager);
-        adapterNguoiDung = new adapter_nguoi_dung(list,getContext());
+        adapterNguoiDung = new adapter_nguoi_dung(list, getContext());
         rcv.setAdapter(adapterNguoiDung);
         // Inflate the layout for this fragment
         binding.flNguoiDung.setOnClickListener(view -> {
             frgAddNguoiDung frgAddNguoiDung = new frgAddNguoiDung();
             FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-            transaction.replace(R.id.frameLayoutMain,frgAddNguoiDung);
+            transaction.replace(R.id.frameLayoutMain, frgAddNguoiDung);
             transaction.addToBackStack(null);
             transaction.commit();
         });
 
-            Bundle bundle = getArguments();
-            if (bundle != null){
-                String tenDangNhap = bundle.getString("tenDangNhap");
-                String matKhau = bundle.getString("matKhau");
-                String hoTen = bundle.getString("hoTen");
-                String email = bundle.getString("email");
-                String soDienThoai = bundle.getString("soDienThoai");
-                String diaChi = bundle.getString("diaChi");
-                int tien = bundle.getInt("soTien");
-                String loaiTaiKhoan = bundle.getString("loaiTaiKhoan");
-                NguoiDung nd = new NguoiDung(tenDangNhap,matKhau,hoTen,email,soDienThoai,diaChi,tien,loaiTaiKhoan);
-                if (nguoiDungDao.checkDangKy(nd)){
-                    list.clear();
-                    list.addAll(nguoiDungDao.getAllNguoiDung());
-                    adapterNguoiDung.notifyDataSetChanged();
-                    Toast.makeText(getContext(), "Thêm người dùng thành công", Toast.LENGTH_SHORT).show();
-                }
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            String tenDangNhap = bundle.getString("tenDangNhap");
+            String matKhau = bundle.getString("matKhau");
+            String hoTen = bundle.getString("hoTen");
+            String email = bundle.getString("email");
+            String soDienThoai = bundle.getString("soDienThoai");
+            String diaChi = bundle.getString("diaChi");
+            int tien = bundle.getInt("soTien");
+            String loaiTaiKhoan = bundle.getString("loaiTaiKhoan");
+            NguoiDung nd = new NguoiDung(tenDangNhap, matKhau, hoTen, email, soDienThoai, diaChi, tien, loaiTaiKhoan);
+            if (nguoiDungDao.checkDangKy(nd)) {
+                list.clear();
+                list.addAll(nguoiDungDao.getAllNguoiDung());
+                adapterNguoiDung.notifyDataSetChanged();
+                Toast.makeText(getContext(), "Thêm người dùng thành công", Toast.LENGTH_SHORT).show();
             }
+        }
 
         return vView;
     }
