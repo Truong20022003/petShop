673fe5da344e8327048ee4f828e0ef6989a52e10
hoang <hoangtnph42058@fpt.edu.vn>
2023-11-22 08:37:00 +0700

    hiển thị đơn hàng

---
+++ app/src/main/java/fpoly/truongtqph41980/petshop/Dao/DonHangChiTietDao.java
@@ -0,0 +1,8 @@
+package fpoly.truongtqph41980.petshop.Dao;
+
+import fpoly.truongtqph41980.petshop.Database.dbHelper;
+
+public class DonHangChiTietDao {
+    private dbHelper dbHelper;
+
+}
--- app/src/main/java/fpoly/truongtqph41980/petshop/Profile.java
+++ app/src/main/java/fpoly/truongtqph41980/petshop/Profile.java
@@ -3,8 +3,10 @@ package fpoly.truongtqph41980.petshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import fpoly.truongtqph41980.petshop.databinding.ActivityProfileBinding;
 
@@ -27,7 +29,7 @@ public class Profile extends AppCompatActivity {
         String diachi = preferences.getString("diachi", "");
         int tien = preferences.getInt("sotien", 0);
         String loaitaikhoan = preferences.getString("loaitaikhoan", "");
-        binding.hiName.setText("Hi, "+hoten);
+        binding.hiName.setText("Hi, " + hoten);
         binding.txtPMaNguoiDung.setText("Mã tài khoản: " + String.valueOf(mand));
         binding.txtPTenDangNhap.setText("Tên đăng nhập: " + tenDN);
         binding.txtPHoTen.setText("Họ tên: " + hoten);
@@ -36,5 +38,11 @@ public class Profile extends AppCompatActivity {
         binding.txtPDiaChi.setText("Địa chỉ: " + diachi);
         binding.txtPSoTien.setText("Số tiền hiện có: " + String.valueOf(tien));
         binding.txtPLoaiTaiKhoan.setText("Loại tài khoản: " + loaitaikhoan);
+        binding.btnPDangXuat.setOnClickListener(view ->
+                startActivity(new Intent(Profile.this, man_hinh_dang_nhap.class))
+        );
+        binding.imgBack.setOnClickListener(view ->
+                startActivity(new Intent(Profile.this, MainActivity.class))
+        );
     }
 }
